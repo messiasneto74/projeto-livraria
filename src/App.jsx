abc64ff58b2401a9b12e9bf198bea4cb2fcb4062
messiasneto74 <netomessias74@gmail.com>
2025-11-03 21:11:51 -0300

    Primeiro Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
-import { useState } from 'react'
-import './styles.css'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./styles.css";
+import CreateBook from "./components/CreateBook";
+import ShowBookList from "./components/ShowBookList";
+import ShowBookDetails from "./components/ShowBookDetails";
+import UpdateBookInfo from "./components/UpdateBookInfo";
 
-function App() {
-  
+const App = () => {
   return (
-    <>
-      <h1>Eu sou o App</h1>
-    </>
-  )
-}
-
-export default App
+    <Router>
+      <div>
+        <Routes>
+          <Route exact path="/" element={<ShowBookList />} />
+          <Route path="/create-book" element={<CreateBook />} />
+          <Route path="/edit-book/:id" element={<UpdateBookInfo />} />
+          <Route path="/show-book/:id" element={<ShowBookDetails />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+};
+export default App;
